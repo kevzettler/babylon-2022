7d405ba6ef00d743a02a5d07684b66f0827d0bf3
Kev Zettler <kevzettler@gmail.com>
2022-09-17 18:05:43 -0700

    install MUDB basics

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { MuWebSocket } from 'mudb/socket/web/client'
+import { MuClient } from 'mudb/client'
 import { Scene, Engine, MeshBuilder, ArcRotateCamera, HemisphericLight, Vector3 } from 'babylonjs';
 import BasicScene from './scenes/basic';
 const canvas:HTMLCanvasElement = document.getElementById('render-target');
@@ -15,3 +17,20 @@ engine.runRenderLoop(function () {
 window.addEventListener("resize", function () {
     engine.resize();
 });
+
+
+// construct client
+const socket = new MuWebSocket({
+    sessionId: Math.random().toString(36).substring(2),
+    url: 'ws://127.0.0.1:9966'
+})
+const client = new MuClient(socket);
+
+
+// start client
+client.start({
+    ready: () => {
+        // send a `world` message to server when client is ready
+        clientChatProtocol.server.message.world('hi everyone')
+    }
+})
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import { Scene, NullEngine, MeshBuilder, ArcRotateCamera, HemisphericLight, Vector3 } from 'babylonjs';
-//Polyfill XMLhttprequest for babylon loader usage
+import http = require('http');
+import { MuWebSocketServer } from 'mudb/socket/web/server'
+import { MuServer } from 'mudb/server'
+
+//Polyfill XMLhttprequest for babylon asset loaderers
 import { XMLHttpRequest } from 'xmlhttprequest';
 global.XMLHttpRequest = XMLHttpRequest;
 
+const socketPort = 9966;
 
+// construct server
+const httpServer = http.createServer()
+const socketServer = new MuWebSocketServer({
+  server: httpServer,
+})
+const server = new MuServer(socketServer);
+
+
+// default scene
 const engine = new NullEngine();
 const scene = new Scene(engine);
-
+const camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new Vector3(0, 0, 0), scene);
 
 engine.runRenderLoop(function() {
     console.log("render loop tick")
@@ -14,4 +28,8 @@ engine.runRenderLoop(function() {
 })
 
 
-console.log("server started...");
+// start server
+server.start()
+httpServer.listen(sockertPort);
+
+console.log("server started on port",  );
