3ed4833ed7dcbf8a913855753f485e041da8e348
Kev Zettler <kevzettler@gmail.com>
2022-09-29 15:41:20 -0700

    Migrate to fixed game loop and render babylon scene in it.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,36 @@
-import { Scene, Engine, MeshBuilder, ArcRotateCamera, HemisphericLight, Vector3 } from '@babylonjs/core';
+import { Scene, Engine, ArcRotateCamera, Vector3 } from '@babylonjs/core';
 import '@babylonjs/loaders';
-import Scene from './scenes/grunt';
-const canvas:HTMLCanvasElement = document.getElementById('render-target');
-const engine = new Engine(canvas, true);
+import SceneDef from './scenes/grunt';
+import Loop from 'fixed-game-loop';
+const canvas = document.getElementById('render-target');
 
-type SceneDef = (engine:Engine) => Promise<Scene>;
+type SceneDef = (engine: Engine) => Promise<Scene>;
 
-// Watch for browser/canvas resize events
-window.addEventListener("resize", function () {
-    engine.resize();
-});
+async function main(sceneDef: SceneDef) {
+    const engine = new Engine(canvas, true);
+    // Watch for browser/canvas resize events
+    window.addEventListener("resize", function() {
+        engine.resize();
+    });
 
-
-async function loadScene(sceneDef:SceneDef){
     const scene = await sceneDef(engine);
     const camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new Vector3(0, 1.5, 0), scene);
     camera.attachControl(canvas, true);
 
-    engine.runRenderLoop(function () {
+
+    engine.runRenderLoop(function() {
         scene.render();
+    });
+
+    new Loop({
+        update: () => { },
+        render: () => scene.render(),
+        autoStart: false,
     });
 }
 
-loadScene(Scene);
+if (!canvas) {
+    throw new Error("canvas render target undefined");
+} else {
+    main(SceneDef);
+}
